9de4d639312a937eb36061c55664d71c643fbfc4
David de Jesús Martínez Mayeta <97991438+imenvyx@users.noreply.github.com>
2025-07-11 15:57:53 -0400

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+import path from "path";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@components": path.resolve(__dirname, "src/components"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
